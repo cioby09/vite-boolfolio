6d10a18af5c4860675c9284f9e48317e87db6786
cioby09 <claudiociobanu09@gmail.com>
2023-06-21 17:05:23 +0200

    Milestone 5: ProjectCard

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
 export default {
+  components: {
+    ProjectCard,
+  },
   data() {
     return {
       projects: []
