e9c62b2fbc43081eb0700103b276434adeb54284
cioby09 <claudiociobanu09@gmail.com>
2023-06-21 17:02:06 +0200

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <script>
+import axios from 'axios';
 export default {
-
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
+        console.log(resp);
+        this.projects = resp.data.result;
+      })
+    }
+  }
 }
 </script>
 
 <template>
-  <h1>Hello Vue</h1>
+  <div class="container">
+    <h1 class="text-center">BoolFolio</h1>
+    <div class="row row-cols-4">
+      <div class="col" v-for="(element, index) in projects" :key="index">
+        <ProjectCard :project="element" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
